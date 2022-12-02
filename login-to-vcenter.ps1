80ced3408cae1d1544e2b4846dd1d010acc0f618
dewab <dwhicker@bifrost.cc>
2022-12-02 09:30:38 -0600

    asdf

--- login-to-vcenter.ps1
+++ login-to-vcenter.ps1
@@ -1,8 +1,9 @@
 $target_vcenter = "ht-vcenter67dc.healthtexas.local"
 $source_vcenter = "gs-dc-vc-01.healthtexas.local"
+$default_username = "Administrator@vsphere.local"
 
-if (!$source_vcenter_creds) { $source_vcenter_creds = Get-Credential }
-if (!$target_vcenter_creds) { $target_vcenter_creds = Get-Credential }
+if (!$source_vcenter_creds) { $source_vcenter_creds = Get-Credential -Message "Source vCenter Creds" }
+if (!$target_vcenter_creds) { $target_vcenter_creds = Get-Credential -Message "Target vCenter Creds" }
 
 try {
     $source = Connect-VIserver -Server $source_vcenter -Credential $source_vcenter_creds
