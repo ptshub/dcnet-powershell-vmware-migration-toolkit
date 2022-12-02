881ca0aedad2fd792f866a595ac3f7d12bcbc52e
dewab <dwhicker@bifrost.cc>
2022-12-02 09:40:12 -0600

    sadf

--- login-to-vcenter.ps1
+++ login-to-vcenter.ps1
@@ -1,17 +1,17 @@
-$source_vcenter = "ht-vcenter67dc.healthtexas.local"
-$target_vcenter = "gs-dc-vc-01.healthtexas.local"
+$global:source_vcenter = "ht-vcenter67dc.healthtexas.local"
+$global:target_vcenter = "gs-dc-vc-01.healthtexas.local"
 $default_username = "Administrator@vsphere.local"
 
 if (!$source_vcenter_creds) { $source_vcenter_creds = Get-Credential -Message "Source vCenter Creds" }
 if (!$target_vcenter_creds) { $target_vcenter_creds = Get-Credential -Message "Target vCenter Creds" }
 
 try {
-    $source = Connect-VIserver -Server $source_vcenter -Credential $source_vcenter_creds
+    $global:source = Connect-VIserver -Server $source_vcenter -Credential $source_vcenter_creds
 } catch {
     Write-Host "Failed to connect to vCenter Server $source_vcenter" -ForegroundColor Red
 }
 try {
-    $target = Connect-VIserver -Server $target_vcenter -Credential $target_vcenter_creds
+    $global:target = Connect-VIserver -Server $target_vcenter -Credential $target_vcenter_creds
 } catch {
     Write-Host "Failed to connect to vCenter Server $target_vcenter" -ForegroundColor Red
 }
